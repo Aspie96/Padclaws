2547715bfa14e4f6bc1630f4c126cf104c57f0a8
Valentino Giudice <valentino.giudice96@gmail.com>
2023-02-21 19:03:38 +0100

    URL shortening improved.

--- components/LinkView.js
+++ components/LinkView.js
@@ -1,19 +1,37 @@
 function shortUrl(url) {
-	if(url.length < 30) {
+	if(url.length < 70) {
 		return url;
 	}
 	const index = url.indexOf("://");
 	const protocol = url.substring(0, index + 3);
 	const resource = url.substring(index + 3);
 	const parts = resource.split("/");
-	if(parts.length <= 2) {
+	var domain = parts[0];
+	if(domain.length > 50) {
+		const subDomains = domain.split(".");
+		domain = "";
+		while(domain.length < 30 && subDomains.length) {
+			domain = subDomains.pop() + "." + domain;
+		}
+		domain = domain.substring(0, domain.length - 1);
+		if(subDomains.length > 0) {
+			domain = "\u2026." + domain;
+		}
+	}
+	if(parts.length == 1) {
 		return url;
 	}
 	var page = parts[parts.length - 1];
-	if(page.length == 0) {
-		page = parts[parts.length - 1] + "/";
+	if(parts.length > 2) {
+		page = "\u2026/" + page;
+		if(page.length == 2) {
+			page = parts[parts.length - 2] + "/";
+		}
 	}
-	return protocol + parts[0] + "/\u2026/" + page;
+	if(page.length > 40) {
+		page = "\u2026" + page.substring(page.length - 20);
+	}
+	return protocol + domain + "/" + page;
 }
 
 export default {
