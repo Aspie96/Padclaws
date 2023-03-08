cf96a7c30781d7e31c3dddccb359f9b0de9b05f4
Valentino Giudice <valentino.giudice96@gmail.com>
2023-03-08 03:29:53 +0100

    Login and write view fixed.

--- components/pages/Login.js
+++ components/pages/Login.js
@@ -1,3 +1,4 @@
+import AlertView from "../AlertView.js"
 import Session from "../../js/session.js"
 
 const nPrivRegex = /^nsec[a-zA-HJ-NP-Z0-9]{59}$/;
@@ -6,10 +7,24 @@ export default {
 	data() {
 		return {
 			privateKey: null,
-			hexPubKey: null,
-			nPubKey: null,
-			Session
+			publicKey: null,
+			Session,
+			showPrivateKey: false
 		};
+	},
+
+	computed: {
+		nPubKey() {
+			if(this.publicKey) {
+				return nostrUtils.encodeEntity(nostrEncEntityPrefixes.npub, this.publicKey);
+			}
+		},
+
+		nSecKey() {
+			if(this.privateKey) {
+				return nostrUtils.encodeEntity(nostrEncEntityPrefixes.nsec, this.privateKey);
+			}
+		}
 	},
 
 	methods: {
@@ -24,7 +39,9 @@ export default {
 		},
 
 		privateKeyInput(privateKey) {
-			console.log("demo");
+			if(privateKey != this.privateKey && privateKey != this.nSecKey) {
+				this.showPrivateKey = false;
+			}
 			if(privateKey.startsWith(nostrEncEntityPrefixes.nsec)) {
 				if(nPrivRegex.test(privateKey)) {
 					const decoded = nostrUtils.decodeEntity(privateKey);
@@ -47,13 +64,23 @@ export default {
 				this.privateKey = null;
 			}
 			if(this.privateKey) {
-				this.hexPubKey = this.Session.toPublicKey(this.privateKey);
-				this.nPubKey = nostrUtils.encodeEntity(nostrEncEntityPrefixes.npub, this.hexPubKey);
+				this.publicKey = this.Session.toPublicKey(this.privateKey);
 			} else {
-				this.hexPubKey = null;
-				this.nPubKey = null;
+				this.publicKey = null;
 			}
+		},
+
+		newKeys() {
+			const keys = nostrUtils.generateKeys();
+			this.privateKey = keys.private;
+			document.getElementById("private_key").value = keys.private;
+			this.publicKey = keys.public;
+			this.showPrivateKey = true;
 		}
+	},
+
+	components: {
+		AlertView
 	},
 
 	template: `
@@ -61,6 +88,7 @@ export default {
 	<div class="text">
 		<p>Each Nostr user has a <i>key pair</i>: a <i>public key</i>, which acts as the public identifier of the user, and the corresponding <i>private key</i>, which is kept secret. The private key gives full access to one's account.</p>
 		<p>Padclaws never shares your private key with anyone and neither should you.</p>
+		<p>If you are a new user of Nostr, you can generate a new key pair.</p>
 		<p>Because of how Nostr works, recovering a lost private key is impossible. Please, store your private key securely.</p>
 	</div>
 	<form @submit.prevent="onSubmit">
@@ -70,15 +98,26 @@ export default {
 		</div>
 		<div class="form-group">
 			<label for="public_key" class="label-output">Hex PubKey</label>
-			<output for="private_key" class="key-output">{{ hexPubKey }}</output>
+			<output for="private_key" class="key-output">{{ publicKey }}</output>
 		</div>
 		<div class="form-group">
 			<label for="public_key_npub" class="label-output">nPubKey</label>
 			<output for="private_key" class="key-output">{{ nPubKey }}</output>
 		</div>
 		<div class="form-buttons">
+			<button type="button" @click="newKeys">New keys</button>
 			<button type="submit" class="btn-submit">Log in</button>
 		</div>
 	</form>
+	<AlertView v-if="showPrivateKey" color="blue" icon="check">
+		<p>Your private key has been generated and is shown below. Store it securely.</p>
+		<p>Your private key can be written in two formats:</p>
+		<dl>
+			<dt>Hex SecKey</dt>
+			<dd>{{ privateKey }}</dd>
+			<dt>nSecKey</dt>
+			<dd>{{ nSecKey }}</dd>
+		</dl>
+	</AlertView>
 	`
 }
