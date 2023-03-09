e124c7e78c15038a15579c843152fe5faf609f10
Valentino Giudice <valentino.giudice96@gmail.com>
2023-03-09 23:28:51 +0100

    Mentions of self highlighted.

--- components/MentionView.js
+++ components/MentionView.js
@@ -1,3 +1,4 @@
+import Session from "../js/session.js"
 import UsersCache from "./UsersCache.js"
 
 export default {
@@ -24,6 +25,12 @@ export default {
 		};
 	},
 
+	computed: {
+		selfUser() {
+			return Session.logged && Session.userKeys.public == this.pubkey;
+		}
+	},
+
 	created() {
 		this.$watch(
 			() => this.event,
@@ -34,7 +41,7 @@ export default {
 
 	methods: {
 		fetchData() {
-			if(this.pubkey) {
+			if(this.event.pubkey) {
 				UsersCache.fetchMetadata(this.pubkey);
 				this.mentionData = UsersCache.users[this.pubkey];
 			}
@@ -43,8 +50,8 @@ export default {
 
 	template: `
 	<template v-if="valid">
-		<RouterLink v-if="mentionData?.metadata?.name" :to="{ name: 'user', params: { pubkey } }" class="mention"><span class="ti ti-at"></span>{{ mentionData.metadata.name  }}</RouterLink>
-		<RouterLink v-else :to="{ name: 'user', params: { pubkey } }" class="mention"><span class="ti ti-at"></span><span class="mention-pubkey">{{ pubkey }}</span></RouterLink>
+		<RouterLink v-if="mentionData?.metadata?.name" :to="{ name: 'user', params: { pubkey } }" class="mention" :class="{ 'mention-self': selfUser }"><span class="ti ti-at"></span>{{ mentionData.metadata.name  }}</RouterLink>
+		<RouterLink v-else :to="{ name: 'user', params: { pubkey } }" class="mention" :class="{ 'mention-self': selfUser }"><span class="ti ti-at"></span><span class="mention-pubkey">{{ pubkey }}</span></RouterLink>
 	</template>
 	<template v-else>{{ mention }}</template>
 	`
